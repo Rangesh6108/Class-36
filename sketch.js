54e7991af78f25b74dc9367534213b400f8622e7
Rangesh6108 <77319427+Rangesh6108@users.noreply.github.com>
2021-04-20 21:15:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var database;
+var canvas;
+
+var gameState=0;
+var playerCount;
+var form,player,game;
+
+function setup(){
+  database = firebase.database();
+  
+  canvas=createCanvas(500,500);
+
+  game=new Game();
+
+  game.getState();
+  game.start();
+  
+}
+
+function draw(){
+  background("white");
+  
+  
+}
+
